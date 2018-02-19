905b5367ed3a95bfdc2a07dd09ccbf73ad5ebe65
John Robinson <john.holland.robinson@gmail.com>
2018-02-19 02:04:21 -0600

    Capitalize First Letter

--- app/helpers/randomize.js
+++ app/helpers/randomize.js
@@ -2,7 +2,7 @@ import { helper } from '@ember/component/helper';
 
 export function randomize([array]/*, hash*/) {
   var random = Math.floor(Math.random() * array.length);
-  var value = array[random];;
+  var value = array[random];
   return value.charAt(0).toUpperCase() + value.slice(1);
 }
 
